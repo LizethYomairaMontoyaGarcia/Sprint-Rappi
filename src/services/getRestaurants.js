f8d622f339b2d5d4f10f0103736a9735ecc00e0e
lizeth montoya <lizethmontoyagarcia@gmail.com>
2023-06-23 15:28:48 -0500

    terminando el proyecto

--- src/services/getRestaurants.js
+++ src/services/getRestaurants.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getRestaurants = async () => {
   try {
-    const { data } = await axios.get("http://localhost:3000/restaurants");
+    const { data } = await axios.get("https://climbing-workable-milkshake.glitch.me/restaurants");
     return data;
   } catch (error) {
     console.log("Error al obtener los restaurantes", error);
